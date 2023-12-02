bcc8e1f25d40b956bae3ee0fd9fad3c2dac99cec
RickjrCode <youngmonk46@gmail.com>
2023-12-01 22:03:47 -0800

    style-search-books

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -4,12 +4,19 @@ import { fetchBooks } from "../api/ajaxHelper";
 
 export default function Books() {
   const [books, setBooks] = useState([]);
+  const [searchBook, setSearchBook] = useState("");
+  const [storedBook, setStoredBook] = useState([]);
   console.log(books);
+
+  const filteredBooks = storedBook.filter((book) => {
+    return book.title.toLowerCase().includes(searchBook.toLowerCase());
+  });
 
   async function getBooks() {
     try {
       const allBooks = await fetchBooks();
       setBooks(allBooks);
+      setStoredBook(allBooks);
     } catch (err) {
       console.error(err);
     }
@@ -21,23 +28,35 @@ export default function Books() {
 
   return (
     <>
-      <div className="books-title">
-        <h2>Book List</h2>
-      </div>
-      <div className="books-container">
-        {books.map((book) => (
-          <div className="book-card">
-            <li key={book.id}>
+      <div className="page-container">
+        <div className="search-bar">
+          <input
+            class="fa-sharp fa-solid fa-magnifying-glass"
+            type="text"
+            placeholder="Search our Library"
+            value={searchBook}
+            onChange={(e) => {
+              setSearchBook(e.target.value.toLowerCase());
+            }}
+          />
+        </div>
+
+        <div className="books-title">
+          <h2>Our Library:</h2>
+        </div>
+        <div className="books-container">
+          {filteredBooks.map((book) => (
+            <div className="book-card" key={book.id}>
               <h3>{book.title}</h3>
+              <img src={book.coverimage} alt={book.title} />
               <p>{book.author}</p>
               <p>{book.description}</p>
-              <img src={book.coverimage} alt={book.title} />
               <p>
                 {book.available ? <strong>"Available"</strong> : "Checked Out"}
               </p>
-            </li>
-          </div>
-        ))}
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
